12a8fb169b09bd8ff3e138fba153bae63783ad2c
Tony Merlin <code@what-the-functor.com>
2023-07-31 20:19:05 +0800

    Add instances for `Pair`
    - `Show[Pair]`
    - `QueryParamEncoder[Pair]`

--- src/main/scala/forex/domain/Rate.scala
+++ src/main/scala/forex/domain/Rate.scala
@@ -1,5 +1,9 @@
 package forex.domain
 
+import cats.Show
+import cats.syntax.all._
+import org.http4s.QueryParamEncoder
+
 case class Rate(
     pair: Rate.Pair,
     price: Price,
@@ -11,4 +15,10 @@ object Rate {
       from: Currency,
       to: Currency
   )
+
+  implicit def showPair(implicit showCurrency: Show[Currency]): Show[Pair] =
+    Show.show { case Pair(c1, c2) => s"${c1.show}${c2.show}" }
+
+  implicit val pairQueryParamEncoder: QueryParamEncoder[Pair] =
+    QueryParamEncoder.fromShow
 }
